545a0ca21ed8e462c60f9d442854049a8cc840fe
miker-bice <mikejerrson.galindez@gmail.com>
2022-04-16 18:17:38 +0800

    objects practice done

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,17 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+// this is the project
+const smartphone = {
+    cores: 8,
+    storage: {
+        ram: '8GB',
+        rom: '128GB'
+    },
+    color: 'black',
+    price: 15000,
+    isSetup: false,
+    isRooted: false,
+    isDiscounted: false
+};
